8a52b0444b905b44da6511fed2164cdf651f9035
GaDaRu <david07tijola@gmail.com>
2023-10-04 13:25:40 +0200

    fixed code

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,17 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Joke;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use function Sodium\add;
 
 class CategoriesController extends AbstractController
 {
